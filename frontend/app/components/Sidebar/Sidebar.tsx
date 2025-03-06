8a6494c9ff73149ba35c7f3369a99a27c4e983c7
samiksha-satthy <satthy.samiksha@gmail.com>
2025-03-06 17:46:37 -0500

    frontend sidebar, radial chart updates

--- frontend/app/components/Sidebar/Sidebar.tsx
+++ frontend/app/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Profile from "../Profile/Profile";
+import RadialChart from "../RadialChart/RadialChart";
 
 function Sidebar(){
     return <div className="w-[20rem] mt-[5rem] h-[calc(100%-5rem)] fixed right-0 top-0 bg-white">
         <Profile/>
+        <div className="mt-4 mx-6">
+            <RadialChart />
+        </div>
     </div>
 }
 
